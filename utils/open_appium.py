5a3af2e5cfbb3e68449f2f1d9fb1cf93624b00c1
1053418947@qq.com <1053418947@qq.com>
2018-11-02 11:48:02 +0800

    add appium cmd

--- utils/open_appium.py
+++ utils/open_appium.py
@@ -1,32 +1,19 @@
 import os
 import yaml
+from time import sleep
 import multiprocessing
-from testcase.page.tests.all_task.all_task_funcs import *
-from testcase.page.learn_center.all_class import AllPage
-from testcase.page.study_center.study_center_main_page import StudyCenter
-from testcase.interface.all_interface import AllInterface
-from testcase.interface.study_center.get_study_center_serverID import GetTaskGroupNum
-from utils.log import logger
-from testcase.page.api.login.login_branch.login_post_step1 import LoginToGetAccessToken
-
-class HomeWork(AllInterface):
-    pass
-
-
-class HomeWork1(StudyCenter, AllPage, GetTaskGroupNum):
-    pass
+# from testcase.page.api.login.login_branch.login_post_step1 import LoginToGetAccessToken
 
 
 def read_dev_cfg():
     devices = []
     new_devices = []
     env = None
-    with open(os.getcwd().split("testcase")[0] + "\config\devices_cfg.yml") as f:
+    with open(os.getcwd().split("utils")[0] + "\config\devices_cfg.yml") as f:
         data = list(yaml.safe_load_all(f))
         for d in data:
             for d1 in d:
                 devices.append(list(d1.items()))
-    logger.info("Old devices: {}".format(devices))
     globals_info = None
     for d in devices:
         for info in d:
@@ -49,11 +36,9 @@ def read_dev_cfg():
                     globals.pop("loginProxy_test")
                     globals.update({"loginProxy": globals.get("loginProxy_cus")})
                 globals_info = globals
-    logger.info("globals_info: {}".format(globals_info))
     for d1 in devices:
         for info1 in d1:
             if info1[0] != "common":
-                logger.info("INFO1: {}".format(info1))
                 for k, v in info1[1].items():
                     for k1, v1 in v.items():
                         if v1 == None:
@@ -62,7 +47,7 @@ def read_dev_cfg():
         for r in result:
             if r[0] != "common":
                 if r[1].get("headers").get("accesstoken") == None:
-                    get_access_token = LoginToGetAccessToken()
+                    # get_access_token = LoginToGetAccessToken()
                     headers = r[1].get('headers')
                     if env.lower() == "test":
                         headers.update({"Host": r[1].get("account").get("loginProxy_test")})
@@ -72,9 +57,9 @@ def read_dev_cfg():
                         common.update({'uname': r[1].get("account").get("uname")})
                         common.update({'pwd': r[1].get("account").get("pwd")})
                         common.update({'loginProxy': headers.get('Host')})
-                        accesstoken = get_access_token.get_access_token(common=common, headers=headers)
-                        logger.info("accesstoken: {}".format(accesstoken))
-                        r[1].get("headers").update({"accesstoken": accesstoken})
+                        # accesstoken = get_access_token.get_access_token(common=common, headers=headers)
+                        # logger.info("accesstoken: {}".format(accesstoken))
+                        # r[1].get("headers").update({"accesstoken": accesstoken})
                         r[1].get("headers").update({"Host": globals_info.get("Host")})
                     if env.lower() != "test":
                         headers.update({"Host": r[1].get("account").get("loginProxy_cus")})
@@ -84,34 +69,29 @@ def read_dev_cfg():
                         common.update({'uname': r[1].get("account").get("uname")})
                         common.update({'pwd': r[1].get("account").get("pwd")})
                         common.update({'loginProxy': headers.get('Host')})
-                        accesstoken = get_access_token.get_access_token(common=common, headers=headers)
-                        logger.info("accesstoken: {}".format(accesstoken))
-                        r[1].get("headers").update({"accesstoken": accesstoken})
+                        # accesstoken = get_access_token.get_access_token(common=common, headers=headers)
+                        # logger.info("accesstoken: {}".format(accesstoken))
+                        # r[1].get("headers").update({"accesstoken": accesstoken})
                         r[1].get("headers").update({"Host": globals_info.get("Host")})
                 new_devices.append([r[1]])
     return new_devices
 
-def main_all(appium_url, d, h, a):
-    while True:
-        try:
-            main1(appium_url, d, h, a)
-        except:
-            pass
-        finally:
-            pass
+def open_appium(appium_url, d, h, a):
+    port = str(appium_url.get("url").split("http://127.0.0.1:")[1]).split("/wd/hub")[0]
+    cmd = "appium -p {} -bp {} --udid {}".format(port, int(port) + 2, d.get("udid"))
+    print("CMD:", cmd)
+    os.system(cmd)
+    sleep(30)
+
+def open_many_appium(new_devices):
+    pool_open_appium = multiprocessing.Pool(processes=2)
+    for device in new_devices:
+        print("Devices:",device)
+        pool_open_appium.apply_async(open_appium, (device[0].get('appium_url'), device[0].get('desired_caps'), device[0].get("headers"), device[0].get('account')))
+    pool_open_appium.close()
+    pool_open_appium.join()
 
 
 if __name__ == '__main__':
     new_devices = read_dev_cfg()
-
-    logger.info("new_devices: {} ".format(new_devices))
-    pool = multiprocessing.Pool(processes=2)
-    for device in new_devices:
-        print(device[0].get('appium_url'))
-        print(device[0].get("headers"))
-        print(device[0].get('desired_caps'))
-        print(device[0].get('account'))
-        pool.apply_async(main_all, (device[0].get('appium_url'), device[0].get('desired_caps'),device[0].get("headers"), device[0].get('account')))
-    pool.close()
-    pool.join()
-
+    open_many_appium(new_devices)
